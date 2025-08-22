7400f5296141420f77558e7bdde608a3d2ce7daa
Christoph Hümmer <huemmer@mpdl.mpg.de>
2025-08-22 16:36:00 +0200

    Implement PuRe-Id capabilities

--- Parsers/TargetParser/PURE.pm
+++ Parsers/TargetParser/PURE.pm
@@ -1,4 +1,4 @@
-#getSelectedFullTxt target parser for MPG.PuRe - V0.3 - (ch) 2025-08-18
+#getSelectedFullTxt target parser for MPG.PuRe - V0.4 - (ch) 2025-08-22
 package Parsers::TargetParser::MPG::PURE;
 
 use base qw(Parsers::TargetParser);
@@ -26,49 +26,98 @@ sub     getSelectedFullTxt {
         my $author =     ($ctx_obj->get('@rft.aulast') && $ctx_obj->get('@rft.aulast')->[0]) ? $ctx_obj->get('@rft.aulast')->[0] : '';
         my $inst   =     $ctx_obj->{'@req.institutes'}->[0] || '';
         
+        my $ou     =     $ctx_obj->get('sfx.openurl');
+        my $pureid =     $ctx_obj->get('PuReId') || '';
+            if (!$pureid && $ou =~ /PuReId/) {
+                $ou =~ m/(?<=PuReId\=)(.+?\d+)/;
+                $pureid = $1;
+                }
+
+
 #########if-loop ggf. wieder entfernen - implementiert für Testzwecke
-        if (!$inst) {
-            my $inst   =     $ctx_obj->get('sfx.institute');
-            }
+#        if (!$inst) {
+#            my $inst   =     $ctx_obj->get('sfx.institute');
+#            }
 
         # read params from config file
         my $config_file         = "mpg_pure.config";
         my $config_parser       = new Manager::Config(file=>$config_file);
         my $host                = $config_parser->getSection('host','url');
+        my $ihost               = $config_parser->getSection('host','item');
+        my $shost               = $config_parser->getSection('host','search');
 
         my $requestaudience = '';
         if ($inst) {
             $requestaudience = $config_parser->getSection('audience', $inst);
             }
 
+
+        # author formatting for API request
         $author =~ s/,.*//g;
+        
 
-####################################################################################################################
-#JSON query object config                                                                                          #
-####################################################################################################################
-        my $header = [
-            'Content-Type' => 'application/json'
-            ];
-
-        my $queryobject = "{\"query\":{\"bool\":{\"must\":[{\"term\":{\"publicState\":{\"value\":\"RELEASED\"}}},{\"term\":{\"versionState\":{\"value\":\"RELEASED\"}}},{\"bool\":{\"must\":          [{\"bool\":{\"must\":[{\"bool\":{\"should\":[{\"match\":{\"metadata.title\":{\"operator\":\"and\",\"query\":\"" . $title . "\"}}},{\"match\":{\"metadata.alternativeTitles.value\":{\"operator\":     \"and\",\"query\":\"" . $title . "\"}}}]}},{\"multi_match\":{\"fields\":[\"metadata.creators.person.familyName\",\"metadata.creators.person.givenName\"],\"operator\":\"and\",\"query\":\"" .         $author . "\",\"type\":\"cross_fields\"}}]}},{\"bool\":{\"should\":[{\"nested\":{\"path\":\"files\",\"query\":{\"bool\":{\"must\":[{\"term\":{\"files.storage\":{\"value\":\"INTERNAL_MANAGED\"}}},   {\"bool\":{\"should\":[{\"term\":{\"files.visibility\":{\"value\":\"AUDIENCE\"}}},{\"term\":{\"files.visibility\":{\"value\":\"PUBLIC\"}}}]}}]}},\"score_mode\":\"avg\"}},{\"nested\":{\"path\":      \"files\",\"query\":{\"bool\":{\"must\":[{\"term\":{\"files.storage\":{\"value\":\"EXTERNAL_URL\"}}}]}},\"score_mode\":\"avg\"}}]}}]}}]}}}";
-
-        my $ua = LWP::UserAgent->new();
-        my $purerequest = HTTP::Request->new('POST', $host, $header, $queryobject);
-        my $pureresponse = $ua->request($purerequest);
-        my $responsejson = $pureresponse->content;
-
-        my $json = JSON->new;
-        my $output = $json->decode($responsejson);
-
-#####################################################################################################################
-#parse file delivery data from response object                                                                      #
-#####################################################################################################################
+        # data structure declaration
         my @filecollection = ();
         my %filedata;
         my @audiences = ();
 
-        foreach my $responserecord (@{$output->{records}}) {
-            foreach my $responsefile (@{$responserecord->{data}->{files}}) {
+
+        # initiate API request
+        if (!$pureid) {
+            my $header = [
+                'Content-Type' => 'application/json'
+                ];
+
+            my $queryobject = "{\"query\":{\"bool\":{\"must\":[{\"term\":{\"publicState\":{\"value\":\"RELEASED\"}}},{\"term\":{\"versionState\":{\"value\":\"RELEASED\"}}},{\"bool\":{\"must\":          [{\"bool\":{\"must\":[{\"bool\":{\"should\":[{\"match\":{\"metadata.title\":{\"operator\":\"and\",\"query\":\"" . $title . "\"}}},{\"match\":{\"metadata.alternativeTitles.value\":{\"operator\":     \"and\",\"query\":\"" . $title . "\"}}}]}},{\"multi_match\":{\"fields\":[\"metadata.creators.person.familyName\",\"metadata.creators.person.givenName\"],\"operator\":\"and\",\"query\":\"" .         $author . "\",\"type\":\"cross_fields\"}}]}},{\"bool\":{\"should\":[{\"nested\":{\"path\":\"files\",\"query\":{\"bool\":{\"must\":[{\"term\":{\"files.storage\":{\"value\":\"INTERNAL_MANAGED\"}}},   {\"bool\":{\"should\":[{\"term\":{\"files.visibility\":{\"value\":\"AUDIENCE\"}}},{\"term\":{\"files.visibility\":{\"value\":\"PUBLIC\"}}}]}}]}},\"score_mode\":\"avg\"}},{\"nested\":{\"path\":      \"files\",\"query\":{\"bool\":{\"must\":[{\"term\":{\"files.storage\":{\"value\":\"EXTERNAL_URL\"}}}]}},\"score_mode\":\"avg\"}}]}}]}}]}}}";
+
+            my $ua = LWP::UserAgent->new();
+            my $purerequest = HTTP::Request->new('POST', $shost, $header, $queryobject);
+            my $pureresponse = $ua->request($purerequest);
+            my $responsejson = $pureresponse->content;
+
+            my $json = JSON->new;
+            my $output = $json->decode($responsejson);
+
+            foreach my $responserecord (@{$output->{records}}) {
+                foreach my $responsefile (@{$responserecord->{data}->{files}}) {
+                    unless ($responsefile->{visibility} eq 'PRIVATE') {
+                        $filedata{'path'} = $responsefile->{content};
+                        $filedata{'contenttype'} = $responsefile->{metadata}->{contentCategory};
+                        $filedata{'availability'} = $responsefile->{visibility};
+
+                        unless ($responsefile->{storage} eq 'EXTERNAL_URL') {
+                            $filedata{'filename'} = $responsefile->{name};
+                            $filedata{'location'} = "INTERNAL";
+                            } else {
+                            $filedata{'location'} = "EXTERNAL";
+                            }
+
+                        if ($responsefile->{visibility} eq 'AUDIENCE') {
+                            $filedata{'audience'} = $responsefile->{allowedAudienceIds};
+                            }
+                        elsif ($responsefile->{visibility} eq 'PUBLIC') {
+                            delete ($filedata{'audience'});
+                            }
+
+                        push @filecollection, {%filedata};
+                    }
+                }
+            }
+
+        } else {
+         
+            $ihost = $ihost . $pureid;
+            my $ua = LWP::UserAgent->new();
+            my $purerequest = HTTP::Request->new('GET', $ihost);
+
+            my $pureresponse = $ua->request($purerequest);
+            my $responsejson = $pureresponse->content;
+            my $json = JSON->new;
+            my $output = $json->decode($responsejson);
+
+            debug $output;
+
+            foreach my $responsefile (@{$output->{files}}) {
                 unless ($responsefile->{visibility} eq 'PRIVATE') {
                     $filedata{'path'} = $responsefile->{content};
                     $filedata{'contenttype'} = $responsefile->{metadata}->{contentCategory};
@@ -79,19 +128,20 @@ sub     getSelectedFullTxt {
                         $filedata{'location'} = "INTERNAL";
                         } else {
                         $filedata{'location'} = "EXTERNAL";
-                        }
+                    }
 
-                    if ($responsefile->{visibility} eq 'AUDIENCE') {
-                        $filedata{'audience'} = $responsefile->{allowedAudienceIds};
-                        }
-                     elsif ($responsefile->{visibility} eq 'PUBLIC') {
-                        delete ($filedata{'audience'});
-                        }
+                if ($responsefile->{visibility} eq 'AUDIENCE') {
+                    $filedata{'audience'} = $responsefile->{allowedAudienceIds};
+                    }
+                elsif ($responsefile->{visibility} eq 'PUBLIC') {
+                    delete ($filedata{'audience'});
+                    }
 
-                    push @filecollection, {%filedata};
+                push @filecollection, {%filedata};
                 }
             }
         }
+
 
 ####################################################################################################################
 #Iterate @filecollection data for preferred delivery                                                               #
@@ -162,8 +212,8 @@ sub     getSelectedFullTxt {
         if ($preferredmatch->{'location'} eq 'EXTERNAL') {
             $url = $preferredmatch->{'path'};
             } else {
-            $host =~ s/\/rest\/items\/search//g;
             $url = $host . $preferredmatch->{'path'};
+            debug "MPG::PURE FullTxtURL is: $url";
             }  
 
 
